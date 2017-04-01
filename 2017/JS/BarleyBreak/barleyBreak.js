d8382a92fd6ed19bb9106766cd6e5c1a014c81ed
vantonenko <vantonenko@sdl.com>
2017-04-02 00:11:12 +0300

    * Replaced recursion with iteration in order to get rid of the stack overflow * Still takes too long to compute

--- 2017/JS/BarleyBreak/barleyBreak.js
+++ 2017/JS/BarleyBreak/barleyBreak.js
@@ -23,23 +23,7 @@ Array.prototype.exchange = function(index1, index2) {
     return newArr;
 }
 
-var printState = function(state) {
-    function* splitIterator(chunkSize) {
-        var cursor = 0;
-        while (cursor < state.length) {
-            var line = state.slice(cursor, cursor + chunkSize);
-            cursor += line.length;
-            yield line;
-        }    
-    }
-
-    var width = Math.floor(Math.sqrt(state.length));
-    for (var line of splitIterator(width)) {
-        console.log(line.join("\t"));
-    }
-}
-
-var solve = function() {
+var barleyBreakSolver = function() {
     var sideLength = 4;
 
     var getStateHash = function(state) {
@@ -51,12 +35,29 @@ var solve = function() {
         return state;
     }
 
+    var getMoves = function() {
+        var directions = ["left", "up", "right", "down"];
+        var vectors = [[-1,0],[0,-1],[1,0],[0,1]];
+
+        var i = 0;
+        return directions.map(direction => { 
+            var vector = vectors[i++];
+            return {
+                direction: direction,
+                vector: {
+                    x: vector[0],
+                    y: vector[1]
+                }
+            };
+        });
+    }
+
     var stateGoal = getStateGoal();
     var stateGoalHash = getStateHash(stateGoal);
 
     var initialState = stateGoal.shuffle();
     var visitState = {};
-    var moves = [[-1,0],[0,1],[1,0],[0,-1]];
+    var moves = getMoves();
     var stateStack = [];
 
     var getZeroItemPosition = function(state) {
@@ -69,50 +70,75 @@ var solve = function() {
     }
 
     var isValidMove = function(itemPosition, move) {
-        var newX = itemPosition.x + move[0];
-        var newY = itemPosition.y + move[1];
-
+        var newX = itemPosition.x + move.vector.x;
+        var newY = itemPosition.y + move.vector.y;
         var isValidMove = newX >= 0 && newX < sideLength &&
             newY >= 0 && newY < sideLength;
-
         return isValidMove;
     }
 
     var applyMove = function(state, itemPosition, move) {
-        var newX = itemPosition.x + move[0];
-        var newY = itemPosition.y + move[1];
-
+        var newX = itemPosition.x + move.vector.x;
+        var newY = itemPosition.y + move.vector.y;
         var newIndex = newY * sideLength + newX;
-
         return state.exchange(newIndex, itemPosition.index);
     }
 
-    var nextStep = function(state) {
-        var stateHash = getStateHash(state);
-
-        if (stateHash == stateGoalHash) return true;
-        if (visitState[stateHash]) return false;
-        
-        visitState[stateHash] = true;
-        stateStack.push(state);
-
-        var position = getZeroItemPosition(state);
-        for (let move of moves.filter(m => isValidMove(position, m))) {
-            var newState = applyMove(state, position, move);
-            var isSolutionFound = nextStep(newState);
-            if (isSolutionFound) return true;
+    this.getPrintState = function(state) {
+        function* splitIterator(chunkSize) {
+            var cursor = 0;
+            while (cursor < state.length) {
+                var line = state.slice(cursor, cursor + chunkSize);
+                cursor += line.length;
+                yield line;
+            }    
         }
 
-        stateStack.pop();
-        return false;
+        var width = Math.floor(Math.sqrt(state.length));
+
+        var printState = [];
+        for (var line of splitIterator(width)) {
+            printState.push(line.map(l => {
+                var sl = `${l}`;
+                return sl + (sl.length > 1 ? "_" : "__");
+            }).join(""));
+        }
+
+        return printState.join("/n");
     }
 
-    nextStep(initialState);
+    this.printState = function(state) {
+        console.log(getPrintState(state));
+    }
 
-    console.log("solution is found:")
-    for (let state of stateStack) {
-        printState(state);
+    this.getStateStack = function() {
+        return stateStack;
+    }
+
+    this.compute = function() {
+        stateStack.push(initialState);
+
+        var interval = setInterval(() => {
+            var state = stateStack.pop();
+            
+            var printState = solver.getPrintState(state);
+            document.querySelector("div").innerHTML = 
+                `${printState.replace(/\/n/g, "<br>")}<br><br>stackSize:${stateStack.length}`;
+
+            var stateHash = getStateHash(state);
+            if (stateHash == stateGoalHash) clearInterval(interval);
+            
+            if (visitState[stateHash]) return;
+            visitState[stateHash] = true;
+
+            var position = getZeroItemPosition(state);
+            var possibleMoves = moves.filter(move => isValidMove(position, move));
+            for (let move of possibleMoves) {
+                var newState = applyMove(state, position, move);
+                stateStack.push(newState);
+            }
+
+            if (stateStack.length == 0) clearInterval(interval);
+        }, 0);
     }
 }
-
-solve();
