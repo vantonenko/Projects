6058f6837566e66b2b3c91cc21f88e11c1d436ad
vantonenko <vantonenko@sdl.com>
2017-04-05 22:59:20 +0300

    * Extract state class

--- 2017/JS/Puzzle15/puzzle15.js
+++ 2017/JS/Puzzle15/puzzle15.js
@@ -2,64 +2,31 @@ var puzzle15Solver = function() {
     var sideLength = 3;
     var moves = 
         [[-1,0],[0,-1],[1,0],[0,1]].map(v => { 
-            return { vector: { x: v[0], y: v[1] } } 
+            return { x: v[0], y: v[1] } 
         });
     
     var stateGoal = (function(){
-            var counter = 0, state = new Array(sideLength * sideLength).fill(0).map(i => counter++);
-            return state;
+            var counter = 0, arr = new Array(sideLength * sideLength).fill(0).map(i => counter++);
+            return new state(arr);
         })();
-
-    var getStateHash = function(state) {
-        return state.join(" ");
-    }
-
-    var stateGoalHash = getStateHash(stateGoal);
 
     var initialState = stateGoal.shuffle();
     var visitState = {};
     var stateStack = [];
 
-    var getZeroItemPosition = function(state) {
-        var zeroItemIndex = state.indexOf(0);
-        return {
-            index: zeroItemIndex,
-            x: zeroItemIndex % sideLength,
-            y: Math.floor(zeroItemIndex / sideLength)
-        };
-    }
-
     var isValidMove = function(itemPosition, move) {
-        var newX = itemPosition.x + move.vector.x;
-        var newY = itemPosition.y + move.vector.y;
+        var newX = itemPosition.x + move.x;
+        var newY = itemPosition.y + move.y;
         var isValidMove = newX >= 0 && newX < sideLength &&
             newY >= 0 && newY < sideLength;
         return isValidMove;
     }
 
-    var applyMove = function(state, itemPosition, move) {
-        var newX = itemPosition.x + move.vector.x;
-        var newY = itemPosition.y + move.vector.y;
-        var newIndex = newY * sideLength + newX;
-        return state.exchange(newIndex, itemPosition.index);
-    }
-
-    var getPrintState = function(state) {
-        var width = Math.floor(Math.sqrt(state.length));
-        return state
-            .splitOnChunks(width)
-            .map(chunk => chunk
-                .map(e => `${e == 0 ? "_" : e}${(e > 9 ? "" : "_")}`)
-                .join("|"))
-            .join("/n");
-    }
-
     var getPossibleMovesIterator = function*(state) {
-        var zeroItemPosition = getZeroItemPosition(state);
         for (let move of moves) {
-            if (!isValidMove(zeroItemPosition, move)) continue;
-            var nextMove = applyMove(state, zeroItemPosition, move);
-            if (visitState[getStateHash(nextMove)]) continue;
+            if (!isValidMove(state.zeroItem, move)) continue;
+            var nextMove = state.applyMove(move);
+            if (visitState[nextMove.hash]) continue;
             yield nextMove;
         }
     }
@@ -67,9 +34,8 @@ var puzzle15Solver = function() {
     var computeIterator = function*() {
         do {
             var state = stateStack.last();
-            var stateHash = getStateHash(state);
-            visitState[stateHash] = true;
-            if (stateHash == stateGoalHash) yield true;
+            visitState[state.hash] = true;
+            if (state.equals(stateGoal)) yield true;
             
             var possibleMoves = getPossibleMovesIterator(state);
             var nextMove = possibleMoves.next();
@@ -89,7 +55,7 @@ var puzzle15Solver = function() {
             return;
         }
 
-        var printState = getPrintState(state);
+        var printState = state.toString();
         element.innerHTML = 
             `${printState.replace(/\/n/g, "<br>")}<br><br>stackSize:${stateStack.length}`;
     }
@@ -99,7 +65,6 @@ var puzzle15Solver = function() {
         visualizeState(initialState, "div#initial");
 
         var iterator = computeIterator();
-
         var interval = setInterval(() => {
             visualizeState(stateStack.last());
             for (let i of new Array(10 * 1000)) {
