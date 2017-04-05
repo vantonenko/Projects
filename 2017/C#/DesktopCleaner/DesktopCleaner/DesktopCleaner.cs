40c49cd3517b76dfc34f523b476fb6a88c3d52f0
vantonenko <vantonenko@sdl.com>
2017-04-05 15:56:50 +0300

    * Put class into a separate file

--- 2017/C#/DesktopCleaner/DesktopCleaner/DesktopCleaner.cs
+++ 2017/C#/DesktopCleaner/DesktopCleaner/DesktopCleaner.cs
@@ -1,8 +1,4 @@
-﻿using System.Collections.Generic;
-using System.Configuration;
-using System.IO;
-using System.Linq;
-using System.ServiceProcess;
+﻿using System.ServiceProcess;
 
 namespace DesktopCleaner
 {
@@ -24,52 +20,6 @@ namespace DesktopCleaner
         protected override void OnStop()
         {
             _processor?.Stop();
-        }
-    }
-
-    public class Processor
-    {
-        private static readonly List<string> FilesToDelete = ConfigurationManager.AppSettings["filesToRemove"].Split(';').ToList();
-        private static readonly string DesktopPath = ConfigurationManager.AppSettings["desktopPath"];
-        private readonly FileSystemWatcher _fileSystemWatcher = new FileSystemWatcher(DesktopPath, "*.*") { NotifyFilter = NotifyFilters.FileName };
-
-        public void Start()
-        {
-            foreach (string file in Directory.EnumerateFiles(DesktopPath).Where(IsFileToDelete))
-            {
-                File.Delete(file);
-            }
-
-            _fileSystemWatcher.Created += FileSystemWatcherOnCreated;
-            _fileSystemWatcher.EnableRaisingEvents = true;
-        }
-
-        private void FileSystemWatcherOnCreated(object sender, FileSystemEventArgs fileSystemEventArgs)
-        {
-            if (!IsFileToDelete(fileSystemEventArgs.Name)) return;
-
-            do
-            {
-                try
-                {
-                    File.Delete(fileSystemEventArgs.FullPath);
-                }
-                catch (IOException exc) when (exc.Message.EndsWith("because it is being used by another process."))
-                {
-                    continue;
-                }
-                break;
-            } while (true);
-        }
-
-        private bool IsFileToDelete(string file)
-        {
-            return FilesToDelete.Contains(Path.GetFileNameWithoutExtension(file));
-        }
-
-        public void Stop()
-        {
-            _fileSystemWatcher.Created -= FileSystemWatcherOnCreated;
         }
     }
 }
