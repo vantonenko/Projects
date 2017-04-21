f6d89fe738246a94819ecfab80c97084337bc2ec
vantonenko <vantonenko@sdl.com>
2017-04-21 14:52:47 +0300

    * Fix tabs

--- 2017/JS/Bridge17/bridge17.js
+++ 2017/JS/Bridge17/bridge17.js
@@ -1,6 +1,6 @@
 var solve = function() {
     var people = [10, 5, 2, 1];
-	var timeGoal = 17;	
+    var timeGoal = 17;    
 
     var getAllPairs = function(arr) {
         var c = [];
@@ -11,50 +11,50 @@ var solve = function() {
         }
         return c;
     }
-	var moves = getAllPairs(people).concat(people.map(e => [e]));	
-	
-	var state = {
+    var moves = getAllPairs(people).concat(people.map(e => [e]));    
+    
+    var state = {
         left: people.slice(),
         right: [],
         isFlashAtLeftSide: true
-    }	
+    }    
 
-	var isValidMove = function(state, participants) {
+    var isValidMove = function(state, participants) {
         var side = state.isFlashAtLeftSide ? state.left : state.right;
         return participants.every(e => side.includes(e));
     }
-	
-	var stateStack = [];	
+    
+    var stateStack = [];    
 
-	var nextStep = function(state, time) {
-		if (time > timeGoal) return false;
-		stateStack.push(state);
-		
-		if (time == timeGoal && state.left.length == 0) {
-			return true;
-        }		
+    var nextStep = function(state, time) {
+        if (time > timeGoal) return false;
+        stateStack.push(state);
+        
+        if (time == timeGoal && state.left.length == 0) {
+            return true;
+        }        
 
-		for (var i = 0; i < moves.length; i++) {
-			var participants = moves[i];
-			if (isValidMove(state, participants)) {				
-				var isSolutionFound = nextStep({
-					left: state.isFlashAtLeftSide ? 
-						state.left.filter(e => !participants.includes(e)) : 
-						state.left.concat(participants),
-					right: !state.isFlashAtLeftSide ? 
-						state.right.filter(e => !participants.includes(e)) : 
-						state.right.concat(participants),
-					isFlashAtLeftSide: !state.isFlashAtLeftSide
+        for (var i = 0; i < moves.length; i++) {
+            var participants = moves[i];
+            if (isValidMove(state, participants)) {                
+                var isSolutionFound = nextStep({
+                    left: state.isFlashAtLeftSide ? 
+                        state.left.filter(e => !participants.includes(e)) : 
+                        state.left.concat(participants),
+                    right: !state.isFlashAtLeftSide ? 
+                        state.right.filter(e => !participants.includes(e)) : 
+                        state.right.concat(participants),
+                    isFlashAtLeftSide: !state.isFlashAtLeftSide
                 }, time + Math.max.apply(Math, participants));
-				
-				if (isSolutionFound) return true;
+                
+                if (isSolutionFound) return true;
             }
         }
-		stateStack.pop();
+        stateStack.pop();
         return false;
-	}
-	nextStep(state, 0);
-	return stateStack;
+    }
+    nextStep(state, 0);
+    return stateStack;
 }
 
 var states = solve();
