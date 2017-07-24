dec48b18c89bef9ad9d5d608cebd716113d1e3e9
vantonenko <vantonenko@sdl.com>
2017-07-24 16:40:39 +0300

    * A snippet to generate huge log file

--- 2017/C#/TraceLogParser/TraceLogParser/Program.cs
+++ 2017/C#/TraceLogParser/TraceLogParser/Program.cs
@@ -1,10 +1,27 @@
-﻿namespace TraceLogParser
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace TraceLogParser
 {
     internal static class Program
     {
         private static void Main()
         {
             new Parser().Run();
+
+            //GenerateHugeLogFile();
+        }
+
+        // ReSharper disable once UnusedMember.Local
+        private static void GenerateHugeLogFile()
+        {
+            var allText = File.ReadAllLines(
+                @"C:\Git\ContentManager\Source\Tools\Misc\TcmTraceLogParser\TraceLogParser\bin\Debug\SampleLogFile\tcm_trace-160MB.log.txt");
+
+            File.WriteAllLines(
+                @"C:\Git\ContentManager\Source\Tools\Misc\TcmTraceLogParser\TraceLogParser\bin\Debug\SampleLogFile\tcm_trace-~MB.log.txt",
+                Enumerable.Range(0, 7).SelectMany(i => allText));
         }
     }
 }
