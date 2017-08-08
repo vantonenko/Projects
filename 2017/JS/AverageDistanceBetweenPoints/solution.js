bc0e46a9230c2008e25a2c36954d8447599ffd78
Vladyslav Antonenko <vantonenko@sdl.com>
2017-08-08 12:11:58 +0300

    * Minor

--- 2017/JS/AverageDistanceBetweenPoints/solution.js
+++ 2017/JS/AverageDistanceBetweenPoints/solution.js
@@ -1,13 +1,13 @@
 var 
     cnt = 100 * 1000, // amount of random line segments
-    N = 3; // amount of dimensions
+    n = 3; // amount of dimensions
 
 // calculates the average distance between to points in N-dimension cube
 // see more at https://www.youtube.com/watch?v=i4VqXRRXi68
 new Array(cnt)
     .fill(0)
     .map(i => Math.sqrt(
-        new Array(3)
+        new Array(n)
             .fill(0)
             .map(i => Math.random(1)-Math.random(1))
             .map(d => d * d)
