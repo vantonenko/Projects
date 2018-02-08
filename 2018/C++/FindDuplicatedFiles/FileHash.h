ea904a34a5a953854036256f5192517eaff3ad8e
vantonenko <vladyslavantonenko@Vladyslavs-MacBook-Pro.local>
2018-02-09 01:10:34 +0200

    * unique_ptr<T[]> isn't capable to return the array size * So still need some wrapper around it

--- 2018/C++/FindDuplicatedFiles/FileHash.h
+++ 2018/C++/FindDuplicatedFiles/FileHash.h
@@ -8,21 +8,39 @@
 
 #include "StringHash.h"
 
+template<class TItem>
+class InMemoryBuffer {
+private:
+    std::unique_ptr<TItem[]> _buffer;
+    long _size;
+public:
+    InMemoryBuffer(long size) : _size(size), _buffer(new TItem[size]) { }
+
+    long Size() const { return _size; }
+    operator TItem* () const { return _buffer.get(); } 
+};
+
 class FileHash {
 public:
     static std::string CalculateHash(const std::string &path) {
-        
+
+        InMemoryBuffer<unsigned char> buf = GetFileContent(path);
+
+        return StringHash::CalculateHash(buf, buf.Size());
+    }
+private:
+    static InMemoryBuffer<unsigned char> GetFileContent(const std::string &path) {
         FILE *fh = fopen(path.c_str(), "r");
         fseek(fh, 0L, SEEK_END);
         
         long fileSize = ftell(fh);
         fseek(fh, 0L, SEEK_SET);
 
-        std::unique_ptr<unsigned char[]> buf(new unsigned char[fileSize]);
+        InMemoryBuffer<unsigned char> buf(fileSize);
         
-        fread(buf.get(), fileSize, 1, fh);
+        fread(buf, fileSize, 1, fh);
         fclose(fh);
 
-        return StringHash::CalculateHash(buf.get(), fileSize);
+        return buf;
     }
 };
