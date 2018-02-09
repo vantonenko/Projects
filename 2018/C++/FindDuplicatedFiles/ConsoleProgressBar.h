c5b733f1d1c3d04d47bae5c502c6f16004e65486
vantonenko <vladyslavantonenko@Vladyslavs-MacBook-Pro.local>
2018-02-09 02:52:32 +0200

    * Make it updating a single line

--- 2018/C++/FindDuplicatedFiles/ConsoleProgressBar.h
+++ 2018/C++/FindDuplicatedFiles/ConsoleProgressBar.h
@@ -6,25 +6,29 @@ class ConsoleProgressBar {
 private:
     int _goalItems;
     int _doneItems;
-    int _donePercentage;
+    int _percentage;
     int _step;
 
 public:
     ConsoleProgressBar(int goalItems, int step = 1) : 
         _goalItems(goalItems), 
         _doneItems(0), 
-        _donePercentage(0),
+        _percentage(0),
         _step(step) { }
     
     void Draw() {
-        std::cout << _donePercentage << "%" << std::endl;
+        std::cout << _percentage << "%\r" << std::flush;
+
+        if (_percentage > 99) {
+            std::cout << std::endl;
+        }
     }
 
     void ReportDoneItem() {
         int newPercentage = ++_doneItems * 100 / _goalItems;
 
-        if (newPercentage > _donePercentage + _step - 1) {
-            _donePercentage = newPercentage;
+        if (newPercentage > _percentage + _step - 1) {
+            _percentage = newPercentage;
             Draw();
         }
     }
