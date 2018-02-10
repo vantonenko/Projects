9e842abf5d61e31bfd4d4882b5fecbad4abffdee
vantonenko <vladyslavantonenko@Vladyslavs-MacBook-Pro.local>
2018-02-10 18:59:16 +0200

    * Extracted timing logic

---
+++ 2018/C++/FindDuplicatedFiles/StopWatch.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#include <time.h>
+
+struct StopWatchResult {
+    int milliseconds;
+};
+
+class StopWatch {
+    StopWatchResult *_result;
+    timespec _ts_beg;
+
+public:
+    StopWatch(StopWatchResult *result): _result(result) {
+        clock_gettime(CLOCK_MONOTONIC, &_ts_beg);
+    }
+
+    ~StopWatch() {
+        timespec ts_end;
+        clock_gettime(CLOCK_MONOTONIC, &ts_end);
+
+        _result->milliseconds = 
+            (ts_end.tv_sec - _ts_beg.tv_sec) * 1000 + 
+            (ts_end.tv_nsec - _ts_beg.tv_nsec) / 1000000;
+    }
+};
